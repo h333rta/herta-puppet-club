0b241f2661eda3195201c4fd43ef4c72492001d8
h333rta <romelleeditz@gmail.com>
2025-04-05 19:28:12 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,20 +7,21 @@ require('dotenv').config();
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
 const client = new TwitterApi({
   appKey: process.env.TWITTER_API_KEY,
   appSecret: process.env.TWITTER_API_SECRET,
 });
 
-// Use deployed URL for Vercel
 const CALLBACK_URL = 'https://herta-puppet-club.vercel.app/callback';
-
-// Store users and their puppet numbers (in memory, resets on cold start)
 let puppetDB = {};
 
-app.use(session({ secret: process.env.SESSION_SECRET || 'herta', resave: false, saveUninitialized: true }));
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'herta',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false, maxAge: 600000 },
+}));
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -31,10 +32,15 @@ app.get('/', (req, res) => {
 });
 
 app.get('/login', async (req, res) => {
-  const { url, oauth_token, oauth_token_secret } = await client.generateAuthLink(CALLBACK_URL);
-  req.session.oauthToken = oauth_token;
-  req.session.oauthTokenSecret = oauth_token_secret;
-  res.redirect(url);
+  try {
+    const { url, oauth_token, oauth_token_secret } = await client.generateAuthLink(CALLBACK_URL);
+    req.session.oauthToken = oauth_token;
+    req.session.oauthTokenSecret = oauth_token_secret;
+    res.redirect(url);
+  } catch (err) {
+    console.error('Login error:', err);
+    res.status(500).send('Failed to generate Twitter auth link.');
+  }
 });
 
 app.get('/callback', async (req, res) => {
@@ -45,28 +51,28 @@ app.get('/callback', async (req, res) => {
     return res.status(400).send('Invalid OAuth request.');
   }
 
-  const loginClient = new TwitterApi({
-    appKey: process.env.TWITTER_API_KEY,
-    appSecret: process.env.TWITTER_API_SECRET,
-    accessToken: oauthToken,
-    accessSecret: oauthTokenSecret,
-  });
-
-  const { client: userClient } = await loginClient.login(oauth_verifier);
-  const user = await userClient.v2.me();
-
-  let puppetNum;
-  if (!puppetDB[user.data.id]) {
-    puppetNum = Object.keys(puppetDB).length + 1;
-    puppetDB[user.data.id] = puppetNum;
-  } else {
-    puppetNum = puppetDB[user.data.id];
-  }
-
-  const paddedNum = String(puppetNum).padStart(4, '0');
-  const newName = `Herta Puppet #${paddedNum}`;
-
   try {
+    const loginClient = new TwitterApi({
+      appKey: process.env.TWITTER_API_KEY,
+      appSecret: process.env.TWITTER_API_SECRET,
+      accessToken: oauthToken,
+      accessSecret: oauthTokenSecret,
+    });
+
+    const { client: userClient } = await loginClient.login(oauth_verifier);
+    const user = await userClient.v2.me();
+
+    let puppetNum;
+    if (!puppetDB[user.data.id]) {
+      puppetNum = Object.keys(puppetDB).length + 1;
+      puppetDB[user.data.id] = puppetNum;
+    } else {
+      puppetNum = puppetDB[user.data.id];
+    }
+
+    const paddedNum = String(puppetNum).padStart(4, '0');
+    const newName = `Herta Puppet #${paddedNum}`;
+
     const imageBuffer = await fetch('https://pbs.twimg.com/media/Gm_D0QZXcAAZti4?format=jpg&name=large').then(res => res.arrayBuffer());
     const b64Image = Buffer.from(imageBuffer).toString('base64');
 
@@ -75,9 +81,13 @@ app.get('/callback', async (req, res) => {
 
     res.send(`<h2>You're now ${newName}!</h2><p>Check your X profile 👀</p>`);
   } catch (err) {
-    console.error(err);
+    console.error('Callback error:', err);
     res.status(500).send('Something went wrong updating your profile.');
   }
 });
+
+// Vercel expects a handler function, not app.listen
+const server = require('http').createServer(app);
+module.exports = (req, res) => server.emit('request', req, res);
 
 module.exports = app;
